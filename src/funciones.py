6559a5198dc3e1af90c5d41e0d5bb319e0bf19ca
BenjaminSoibelzon <benjamin.29112006@gmail.com>
2026-04-03 20:46:20 -0300

    punto 1

--- src/funciones.py
+++ src/funciones.py
@@ -1,2 +1,23 @@
-def saludar(nombre):
-    return f"hola {nombre}"
+def analizar_texto(texto: str):
+    """
+    Recibe un string multilínea y devuelve:
+    - total_lineas
+    - total_palabras
+    - promedio
+    - lista de tuplas (linea, cantidad_palabras) por encima del promedio
+    """
+    lineas = texto.split('\n')
+    total_lineas = len(lineas)
+
+    palabras_en_cada_linea = [linea.split() for linea in lineas]
+    total_palabras = sum(len(p) for p in palabras_en_cada_linea)
+
+    promedio = total_palabras / total_lineas if total_lineas > 0 else 0
+
+    lineas_sobre_promedio = []
+    for linea in lineas:
+        cant = len(linea.split())
+        if cant > promedio:
+            lineas_sobre_promedio.append((linea, cant))
+
+    return total_lineas, total_palabras, promedio, lineas_sobre_promedio
